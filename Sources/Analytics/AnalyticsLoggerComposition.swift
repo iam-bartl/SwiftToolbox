3a8ae6c49158485028045de3d0cf590da95c419b
Yauhen Rusanau <yauhen.rusanau@gmail.com>
2024-03-29 20:01:36 +0200

    Add shared params for analytics logger composition

--- Sources/Analytics/AnalyticsLoggerComposition.swift
+++ Sources/Analytics/AnalyticsLoggerComposition.swift
@@ -9,14 +9,29 @@ import Foundation
 
 public class AnalyticsLoggerComposition {
     private let services: [AnalyticsLogger]
+    private let sharedParams: (() -> [String: Any]?)?
     
-    public init(services: [AnalyticsLogger]) {
+    public init(services: [AnalyticsLogger], sharedParams: (() -> [String: Any]?)? = nil) {
         self.services = services
+        self.sharedParams = sharedParams
     }
 }
 
 extension AnalyticsLoggerComposition: AnalyticsLogger {
     public func logEvent(_ event: AnalyticsEvent) {
-        services.forEach { $0.logEvent(event) }
+        let commonParams = sharedParams?() ?? [:]
+        let eventParams = event.params ?? [:]
+        
+        let allParams = commonParams.merging(eventParams, uniquingKeysWith: { _, new in new })
+        
+        let newEvent = ComposedEvent(name: event.name, params: allParams)
+        services.forEach { $0.logEvent(newEvent) }
+    }
+}
+
+private extension AnalyticsLoggerComposition {
+    struct ComposedEvent: AnalyticsEvent {
+        let name: String
+        let params: [String: Any]?
     }
 }
