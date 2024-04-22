d53a50bac771a62340e641737d74da204c868cce
Yauhen Rusanau <yauhen.rusanau@gmail.com>
2024-04-22 21:41:00 +0300

    Add types for analytics events

--- Sources/Analytics/AnalyticsEvent.swift
+++ Sources/Analytics/AnalyticsEvent.swift
@@ -7,7 +7,19 @@
 
 import Foundation
 
+public enum AnalyticsEventType: String {
+    case regular
+    case nonfatal
+}
+
 public protocol AnalyticsEvent {
+    var type: AnalyticsEventType { get }
     var name: String { get }
     var params: [String: Any]? { get }
+}
+
+public extension AnalyticsEvent {
+    var type: AnalyticsEventType {
+        .regular
+    }
 }
--- Sources/Analytics/AnalyticsLoggerComposition.swift
+++ Sources/Analytics/AnalyticsLoggerComposition.swift
@@ -20,11 +20,11 @@ public class AnalyticsLoggerComposition {
 extension AnalyticsLoggerComposition: AnalyticsLogger {
     public func logEvent(_ event: AnalyticsEvent) {
         let commonParams = sharedParams?() ?? [:]
-        let eventParams = event.params ?? [:]
+        let eventParams = event.allParams
         
         let allParams = commonParams.merging(eventParams, uniquingKeysWith: { _, new in new })
         
-        let newEvent = ComposedEvent(name: event.name, params: allParams)
+        let newEvent = ComposedEvent(type: event.type, name: event.name, params: allParams)
         services
             .filter { $0.isEnabled }
             .forEach { $0.logEvent(newEvent) }
@@ -33,7 +33,20 @@ extension AnalyticsLoggerComposition: AnalyticsLogger {
 
 private extension AnalyticsLoggerComposition {
     struct ComposedEvent: AnalyticsEvent {
+        let type: AnalyticsEventType
         let name: String
         let params: [String: Any]?
+    }
+}
+
+private extension AnalyticsEvent {
+    var defaultParams: [String: Any] {
+        [
+            "event_type": type.rawValue
+        ]
+    }
+    
+    var allParams: [String: Any] {
+        defaultParams.merging(params ?? [:], uniquingKeysWith: { old, _ in old })
     }
 }
