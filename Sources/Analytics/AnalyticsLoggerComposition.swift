0765477843cb522aaef4386b603ab783be33c7ee
Yauhen Rusanau <yauhen.rusanau@gmail.com>
2024-11-22 18:49:00 +0200

    Swift 6 updates

--- Sources/Analytics/AnalyticsLoggerComposition.swift
+++ Sources/Analytics/AnalyticsLoggerComposition.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public class AnalyticsLoggerComposition {
+public final class AnalyticsLoggerComposition {
     private let services: [AnalyticsLogger]
     private let sharedParams: (() -> [String: Any]?)?
     
