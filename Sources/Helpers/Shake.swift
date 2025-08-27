b7e2434587b2e1363dc400c5f1f31657b5641f14
Yauhen Rusanau <yauhen.rusanau@gmail.com>
2025-08-27 08:45:35 +0300

    add shake gesture modifier

---
+++ Sources/Helpers/Shake.swift
@@ -0,0 +1,34 @@
+#if os(iOS)
+
+import SwiftUI
+
+private extension UIDevice {
+    static let deviceDidShakeNotification = Notification.Name(rawValue: "deviceDidShakeNotification")
+}
+
+extension UIWindow {
+     open override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            NotificationCenter.default.post(name: UIDevice.deviceDidShakeNotification, object: nil)
+        }
+     }
+}
+
+private struct DeviceShakeViewModifier: ViewModifier {
+    let action: () -> Void
+
+    func body(content: Content) -> some View {
+        content
+            .onReceive(NotificationCenter.default.publisher(for: UIDevice.deviceDidShakeNotification)) { _ in
+                action()
+            }
+    }
+}
+
+public extension View {
+    func onShake(perform action: @escaping () -> Void) -> some View {
+        modifier(DeviceShakeViewModifier(action: action))
+    }
+}
+
+#endif
